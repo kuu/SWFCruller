2ea0f984dbfe1e56bf679cbb560387e99d23936a
Kuu MIYAZAKI <kuu.miyazaki@gree.net>
2013-03-06 15:56:22 +0900

    Added the pause/resume button.

--- README.md
+++ README.md
@@ -7,12 +7,13 @@ SWFCruller extends the Chrome Developer Tools for debugging [SWFCrew](https://gi
 
 Features
 --------
-  - Displaying of character-id and type of the display objects currently on the stage.
+  - Displaying of the character-id and the type of the display objects currently on the stage.
+  - Pausing/Reuming of the playback of the SWF file.
 
 Usage
 ------
   - Clone this repository.
-  - Add 'extensions' directory to your Google Chrome browser as an unpacked extension (see the [tutorial](http://developer.chrome.com/extensions/getstarted.html#unpacked).)
+  - Add 'extension' directory to your Google Chrome browser as an unpacked extension (see the [tutorial](http://developer.chrome.com/extensions/getstarted.html#unpacked).)
   - Launch the browser and open up a web page that uses swfcrew.js
   - Open the Chrome Developer Tools window. (Note that this causes the web page to reload to inject some JavaScript code.)
   - Choose 'SWFCruller' tab in the devtools panels, then you can see some inspected data on the panel.
--- extension/devtools.js
+++ extension/devtools.js
@@ -4,6 +4,7 @@ chrome.devtools.panels.create(
   "panel.html",
   function (panel) {
     var code = [
+        "window.stage = null;",
         "window.addEventListener('message', function(event) {",
           "if (event.source != window) return;",
           "var message = event.data;",
@@ -11,10 +12,30 @@ chrome.devtools.panels.create(
           "console.log('[Web page] postMessage received.', message);",
           "if (message.type === 'echo') {",
             "window.postMessage(JSON.stringify({from: 'webpage', type: 'echo'}), '*');",
+          "} else if (message.type === 'togglePlay') {",
+            "if (!stage) return;",
+            "if (stage.isOpen) {",
+              "stage.close();",
+            "} else {",
+              "stage.open();",
+            "}",
+            "window.postMessage(JSON.stringify({",
+              "from: 'webpage', type: 'playbackstate',",
+              "data: (stage.isOpen ? 'playing' : 'stopped')",
+            "}), '*');",
           "} else if (message.type === 'inject') {",
           "if (window.theatre) {",
-            "var actions = theatre.crews.swf.actions;",
-            "var actors = theatre.crews.swf.actors;",
+            "var swfcrew = window.theatre.crews.swf;",
+            "var actions = swfcrew.actions;",
+            "var actors = swfcrew.actors;",
+            "var Player  = swfcrew.Player;",
+            "var WrapperPlayer = function () {",
+              "Player.apply(this, arguments);",
+              "window.stage = this.stage;",
+            "};",
+            "WrapperPlayer.prototype = Object.create(Player.prototype);",
+            "WrapperPlayer.prototype.constructor = WrapperPlayer;",
+            "swfcrew.Player = WrapperPlayer;",
             "var origMethods = {",
               "add: actions.add,",
               "replace: actions.replace,",
@@ -33,6 +54,7 @@ chrome.devtools.panels.create(
                     "tId = tActor.displayListId;",
                     "tType = tActor.displayListType;",
                   "}",
+                  "tType = tType ? tType.substring(6) : '';",
                   "messageData.data = {",
                     "action: pAction,",
                     "parent: pSpriteActor.displayListId,",
@@ -50,28 +72,59 @@ chrome.devtools.panels.create(
             "overrideActions('remove');",
           "}",
           "}",
-        "}, false);"
+        "}, false);",
+        "window.onload = function () {",
+          "window.postMessage(JSON.stringify({from: 'webpage', type: 'loaded'}), '*');",
+        "}"
       ];
 
     chrome.devtools.inspectedWindow.reload({
       injectedScript: code.join('')
     });
 
+    // Establish a connection with the background page.
     var port = chrome.extension.connect();
+
+    // Create buttons.
+    var playbackButton = panel.createStatusBarButton('stop.jpg', 'pause/resume the playback of SWF', false);
+    playbackButton.onClicked.addListener(function () {
+      port.postMessage({from:'devtools', type: 'togglePlay'});
+    });
+
+    port.onMessage.addListener(function f (message) {
+      if (message.type === 'loaded') {
+        // Trigger the injection to the web page 
+        //  so that it can overwrite the existing SWFCrew methods.
+        port.postMessage({from:'devtools', type: 'inject'});
+      }
+    });
+
+    var initialized = false;
+
     panel.onShown.addListener(function (window) {
-      port.postMessage({from:'devtools', type: 'inject'});
+
+      if (initialized) {
+        return;
+      }
+
+      // Show the root sprite.
       var document = window.document;
       root = document.getElementById('0');
       root.innerHTML = 'id: 0 (RootSprite)';
       root.style.position = 'relative';
       root.style.color = 'blue';
+
+      // The element added to the screen at last.
+      var lastAdded = null;
+
+      // Set the UI listeners.
       port.onMessage.addListener(function (message) {
         if (message.from !== 'webpage') return;
         var data = message.data;
         if (message.type === 'echo') {
-          var nodeList = document.querySelectorAll('*');
-          alert('NodeList.length=' + nodeList.length);
+          ;
         } else if (message.type === 'actions') {
+          // Display the data inspected.
           var parent = document.getElementById(data.parent);
           if (data.action === 'add') {
             var elem = document.createElement('div');
@@ -79,13 +132,26 @@ chrome.devtools.panels.create(
             elem.innerHTML = 'id: ' + data.target + ' (' + data.type + ')';
             elem.style.position = 'relative';
             elem.style.marginLeft = '20px';
-            elem.style.color = 'blue';
+            elem.style.color = 'red';
+            if (lastAdded && lastAdded.style) {
+              lastAdded.style.color = 'blue';
+            }
+            lastAdded = elem;
             parent.appendChild(elem);
           } else {
-            var elem= document.getElementById(data.target);
+            var elem = document.getElementById(data.target);
+            if (lastAdded === elem) lastAdded = null;
             parent.removeChild(elem);
+          }
+        } else if (message.type === 'playbackstate') {
+          // Switch the playback button.
+          if (data === 'stopped') {
+            playbackButton.update('play.jpg', null, false);
+          } else {
+            playbackButton.update('stop.jpg', null, false);
           }
         }
       });
+      initialized = true;
     });
   });
